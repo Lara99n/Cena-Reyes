be6b6db4258590ab8a0fb29e646bb7635f7538b7
Lara99n <91375775+Lara99n@users.noreply.github.com>
2022-05-23 17:06:05 -0300

    ok

---
+++ src/lara.js
@@ -0,0 +1,37 @@
+const lara = () => {
+  const reyes = [
+    {
+      nombre: "Atanagildo",
+      reinado: 02,
+      vacasComidas: 49,
+    },
+    {
+      nombre: "Ataúlfo",
+      reinado: 722,
+      vacasComidas: 744,
+    },
+    {
+      nombre: "Ervigio",
+      reinado: 222,
+      vacasComidas: 644,
+    },
+    {
+      nombre: "Leogivildo",
+      reinado: 222,
+      vacasComidas: 414,
+    },
+    {
+      nombre: "Recesvinto",
+      color: "royalblue",
+      reinado: 232,
+      vacasComidas: 434,
+    },
+    {
+      nombre: "Sisebuto",
+      reinado: 22,
+      vacasComidas: 44,
+    },
+  ];
+
+  return <div>{resultado1}</div>;
+};
